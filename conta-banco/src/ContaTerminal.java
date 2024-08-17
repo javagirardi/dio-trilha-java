5e1d403b17648ad08575034e176a566b88316889
Javan Girardi <javan.girardi@gmail.com>
2024-08-17 09:19:13 -0300

    sistema de cadastro de cliente em Banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {       
+
+        Scanner sc = new Scanner(System.in); 
+        sc.useLocale(Locale.US);       
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        int numero = sc.nextInt();
+        System.out.println("Por favor, digite o nome da Agência: ");
+        String nomeAgencia = sc.next();
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = sc.next();      
+        System.out.println("Por favor, digite o seu saldo: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + ". Obrigado por criar uma conta em nosso banco. Sua agência é " 
+        + nomeAgencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
+
     }
 }
